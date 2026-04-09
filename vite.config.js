f8f806effc71b52fe3f91f6983715760b7b816e1
jameelshakrna-max <jameelshakrna@gmail.com>
2026-04-10 01:46:05 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
+  server: { port: 3000 },
+  root: path.resolve(__dirname),
+  css: { postcss: { plugins: [] } }
 })
