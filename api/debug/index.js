0a15c8f3af5af5b48ca49d4b119ecf7707bfdd46
jameelshakrna-max <jameelshakrna@gmail.com>
2026-06-22 13:03:46 +0300

    debug: add getUser call to debug endpoint to isolate auth module issue

--- api/debug/index.js
+++ api/debug/index.js
@@ -1,10 +1,13 @@
+import { getUser } from '../_auth.js'
+
 export const runtime = 'nodejs'
 
 export async function GET(req) {
+  const user = await getUser(req)
   const body = JSON.stringify({
     method: req.method,
     url: req.url,
-    headers: Object.fromEntries(req.headers),
+    hasUser: !!user,
     runtime: 'nodejs',
     ok: true,
   })
@@ -14,10 +17,12 @@ export async function GET(req) {
 }
 
 export async function POST(req) {
+  const user = await getUser(req)
   const text = await req.text()
   const body = JSON.stringify({
     method: req.method,
     url: req.url,
+    hasUser: !!user,
     contentLength: text.length,
     runtime: 'nodejs',
     ok: true,
